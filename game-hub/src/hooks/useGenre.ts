d96d045b99740aefedb9189866bfd5d96e934490
Julian Odilibe <graftwerx@gmail.com>
2024-10-04 13:40:55 +0100

    fetch genre build

---
+++ game-hub/src/hooks/useGenre.ts
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+
+export interface PlatformProps {
+    id:number;
+    name: string;
+    slug: string;
+}
+
+export interface Fetched {
+    id: number;
+    name: string;
+    background_image:string;
+    parent_platforms:{platform: PlatformProps}[]
+    metacritic:number;
+  }
+  
+  interface FetchProps {
+    count: number;
+    results: Fetched[];
+  }
+const useGenres = () => {
+    const [genres, setGenres] = useState<Fetched[]>([]);
+    const [error, setError] = useState("");
+    const [isLoading, setLoading] = useState(false);
+  
+    useEffect(() => {
+        const controller = new AbortController();
+        setLoading(true);
+      apiClient
+        .get<FetchProps>("/genres", {signal: controller.signal})
+        .then((res) => {
+          setGenres(res.data.results);
+          setLoading(false);})
+        .catch((err) => {
+            if(err instanceof CanceledError)return;
+            setError(err.message)
+            setLoading(false);
+          });
+
+        return () => controller.abort();
+    }, []);
+    return {genres,error, isLoading};
+}
+
+export default useGenres;
